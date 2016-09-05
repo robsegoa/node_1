896f9a6270363dd5df3ba3522c07a7029b2a60f9
Robson Michkinis Segoa <robson.segoa@neoway.com.br>
2016-09-05 10:22:32 -0300

    Retirada do Waib, pois estou criando um projeto só para o waib!

--- waibtec/routes/home.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-	
-	var home = app.controllers.home;
-
-	app.get('/', home.index);
-
-}
--- waibtec/routes/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-	
-	var user = app.controllers.user;
-
-	app.get('/user', user.index);
-
-}
